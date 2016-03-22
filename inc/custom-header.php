cb7a1f334db16012074d7d5bb0ef73ae189087d6
pandabrand <pandabrand@gmail.com>
2016-03-21 19:56:40 -0500

    changing all _s to _pandapress

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -12,33 +12,33 @@
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
  *
- * @package _s
+ * @package _pandapress
  */
 
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses _s_header_style()
+ * @uses _pandapress_header_style()
  */
-function _s_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( '_s_custom_header_args', array(
+function _pandapress_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( '_pandapress_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
-		'wp-head-callback'       => '_s_header_style',
+		'wp-head-callback'       => '_pandapress_header_style',
 	) ) );
 }
-add_action( 'after_setup_theme', '_s_custom_header_setup' );
+add_action( 'after_setup_theme', '_pandapress_custom_header_setup' );
 
-if ( ! function_exists( '_s_header_style' ) ) :
+if ( ! function_exists( '_pandapress_header_style' ) ) :
 /**
  * Styles the header image and text displayed on the blog.
  *
- * @see _s_custom_header_setup().
+ * @see _pandapress_custom_header_setup().
  */
-function _s_header_style() {
+function _pandapress_header_style() {
 	$header_text_color = get_header_textcolor();
 
 	/*
