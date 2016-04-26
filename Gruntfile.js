f10cdda742ec59d1d39ce4b90d5810e2cd58c7ba
pandabrand <pandabrand@gmail.com>
2016-04-26 16:01:06 -0500

    merge from mtac

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,64 @@
-//Project Configuration
-grunt.initConfig({
-  pkg. grunt.file.readJSON('package.json'),
-  mkdir: {
-    all: {
-      options: {
-        create: ['assets/css', 'assets/js','assets/img']
-      }
-    }
-  }
+module.exports = function(grunt) {
+  //Project Configuration
+  grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	mkdir: {
+	  all: {
+		options: {
+		  create: ['assets/css', 'assets/js','assets/img']
+		}
+	  }
+	},
+	sass: {
+	  dist: {
+	    options: {
+	      require: 'susy',
+	    },
+	    files: {
+	      'assets/css/style.css' : 'sass/style.scss'
+	    }
+	  }
+	},
+	imagemin: {
+	   dist: {
+		  options: {
+			optimizationLevel: 5
+		  },
+		  files: [{
+			 expand: true,
+			 cwd: 'img',
+			 src: ['*.{png,jpg,gif}'],
+			 dest: 'assets/img'
+		  }]
+	   }
+	},
+	uglify: {
+	  target: {
+	    files: {
+	      'assets/js/app.min.js': 'src/js/*.js'
+	    }
+	  }
+	},
+	watch: {
+	  css: {
+		files: ['sass/**/*.scss'],
+		tasks: ['sass']
+	  },
+	  imagemin: {
+	    files: ['img/*.{png,jpg,gif}'],
+	    tasks: ['imagemin']
+	  },
+	  uglify: {
+	    files: ['src/js/*.js'],
+	    tasks: ['uglify']
+	  }
+	}
+  });
   
-  grunt.loadNpmTask('grunt-mkdir');
-  
-  grunt.registerTask('default', ['mkdir']);
-});
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['sass, imagemin, uglify']);
+};
